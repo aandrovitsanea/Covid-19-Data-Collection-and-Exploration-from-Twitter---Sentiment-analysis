c038b3664c661abb3388588d11a0840b40bc627e
aandrovitsanea <anna.androvitsanea@th-luebeck.de>
2021-04-03 23:29:48 +0300

    add config file for twitter account

---
+++ twitter_config.py
@@ -0,0 +1,7 @@
+config = {
+    'api_key': 'add_your__here',
+    'api_secret_key': 'add_your_api_secret_key_here',
+
+    'access_token': 'add_your_access_token_here',
+    'access_token_secret': 'add_your_access_token_secret_here'
+}
